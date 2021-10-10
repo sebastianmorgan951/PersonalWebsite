a0b323197be18f9dc5e56c9d6f6c057ad115d2a4
sebastianmorgan951 <sebastianmorgan951@gmail.com>
2021-10-10 16:54:16 -0700

    Fixing viewport

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import styles from "../styles/main.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
